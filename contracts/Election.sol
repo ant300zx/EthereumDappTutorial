bef609d4cb63cdb68d3ee72a43d792a04df5cbf1
Anthony Cull <anthony@concreteplatform.com>
2018-12-21 17:44:10 +0000

    list candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,29 @@
 pragma solidity ^0.5.0;
 
 contract Election {
-  //  Store candidate
-  //  Read candidate
-  string public candidate;
-  //  Constructor
-  constructor() public {
-    candidate = "Candidate 1";
-  }
+    // Model a Candaidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Store Candidates
+
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    //  Constructor
+    constructor() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
 }
